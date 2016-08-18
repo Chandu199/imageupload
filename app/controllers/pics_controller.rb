e9a334ea8cefd43c211a5b5183700b755c8f9f48
chandrakanth <n.chandrakanth0@gmail.com>
2016-08-18 07:16:40 +0000

    added all pic controller and actions

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,30 @@
 class PicsController < ApplicationController
+    before_action :find_pic, only:[:show,:edit,:update,:destroy]
+    
     def index
+        @pics = Pics.all
+    end
+    def new
+        @pic  = Pic.new
+    end
+    def create
+        @pic  = Pic.new(pic_params)
+        if @pic.save
+            redirect_to @pic, notice: 'Your Pictures is saved'
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        
+    end
+    
+    private
+    def pic_params
+        params.require(:pic).permit(:title,:description)
+    end
+    def find_pic
+        @pic = Pic.find(params[:id])
     end
 end
